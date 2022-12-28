6d96bbc43bf5fea7186aee655ed580cd2249767e
MonicaTeixeira <kinhamiranda13@gmail.com>
2022-12-28 13:55:57 +0200

    Iniciar o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './frontend/login/login.component';
+import { PerfilComponent } from './frontend/perfil/perfil.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PerfilComponent,
   ],
   imports: [
     BrowserModule,
