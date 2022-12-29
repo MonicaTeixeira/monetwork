5a3a82f25bc5264e51cf29ebbd7d3ebe0c36aff0
MonicaTeixeira <kinhamiranda13@gmail.com>
2022-12-29 18:51:29 +0200

    criei um file json

--- src/app/frontend/newuser/newuser.component.ts
+++ src/app/frontend/newuser/newuser.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./newuser.component.scss']
 })
 export class NewuserComponent {
+
+  username!: string
+  email!: string
+  password!: string
+
+  constructor(private http: HttpClient) { }
+
+  register() {
+    const username = this.username
+    const email = this.email
+    const password = this.password
+
+    const newUser = { username, email, password }
+    
+  }
 
 }
