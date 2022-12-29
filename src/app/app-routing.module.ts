64e4b95b7a37401597488b35909910a6777d4f98
MonicaTeixeira <kinhamiranda13@gmail.com>
2022-12-29 17:27:46 +0200

    Criei o componente perfil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './frontend/login/login.component';
 import { NewuserComponent } from './frontend/newuser/newuser.component';
+import { PerfilComponent } from './frontend/perfil/perfil.component';
 
 const routes: Routes = [
-  // { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },  
-  { path: 'newuser', component: NewuserComponent }
+  { path: 'newuser', component: NewuserComponent },
+  { path: 'perfil', component: PerfilComponent }
 ];
 
 @NgModule({
