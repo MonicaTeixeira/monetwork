35dbe6e18ec8976c48d14d9ae0d2dc9f34e83964
MonicaTeixeira <kinhamiranda13@gmail.com>
2022-12-30 12:03:43 +0200

    dados do formulario cadastro validados

--- src/app/backend/server.js
+++ src/app/backend/server.js
@@ -3,21 +3,22 @@ const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
 
+// Criar servers de express
 const server = express()
-
+// Routes e middleware para o server local json
 const usersPath = path.join(__dirname, 'users', 'users.json');
+// Campo parse JSON (como enviados por clientes API)
 server.use(bodyParser.json());
-
+// Ler users do users.json
 let users = fs.readFileSync(usersPath, 'utf8');
 users = JSON.parse(users);
-
+//Cors para postman
 let cors = require("cors");
 server.use(cors());
 
 server.get('/', (req, res) => {
     res.send('Teste!!!')
 })
-
 server.get('/users', (req, res) => {
     res.send(users)
 })
