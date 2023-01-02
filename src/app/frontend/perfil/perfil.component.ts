e620381c542e3500a429c96ff98bf5bd91ff9496
MonicaTeixeira <kinhamiranda13@gmail.com>
2023-01-02 20:28:05 +0200

    layout pagina perfil

--- src/app/frontend/perfil/perfil.component.ts
+++ src/app/frontend/perfil/perfil.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-perfil',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class PerfilComponent {
   
+  @Input() name: string = ''
+
+  
+
 }
